822631b7e687c384b6873ce31039a095c96b81e5
Deusdete Filho <deusdetefilho@gmail.com>
2019-04-06 23:00:24 -0300

    Atualizacoes

---
+++ models/db.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(process.env.MYSQL_DB,process.env.MYSQL_USER,process.env.MYSQL_PASS, {
+  host: process.env.MYSQL_HOST,
+  dialect: "mysql"
+})
+sequelize.authenticate().then(function(){
+  console.log("Conectado com sucesso")
+}).catch(function(erro){
+  console.log("Falhao ao se conectar" +erro)
+})
+
+module.exports = {
+  Sequelize: Sequelize,
+  sequelize: sequelize
+}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 var bodyParser = require('body-parser')
+
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
@@ -9,148 +10,268 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
+app.all('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
 var mysql = require("mysql");
 
 app.post('/botfilme', function(request, response) {
-  
+
 
   var connection = mysql.createConnection({
       host     : process.env.MYSQL_HOST,
       user     : process.env.MYSQL_USER,
       password : process.env.MYSQL_PASS,
-      database : process.env.MYSQL_DB  
+      database : process.env.MYSQL_DB
   });
   connection.connect();
-  
-  var intentName = request.body.queryResult.intent.displayName;  
-  
-  
-// ---- - - - - - - -- -- - - - - - - - - - - -- - - - - - - - - --- - - - - - - - - - - - - --   
-    
-if ( intentName == "indicar-filme"  ) 
-  {
- console.log('Entrou no intent -> indicar-filme')  
-  
-    var nome_genero = request.body.queryResult.parameters['nome-genero'];
-    var query = 'select * from filme where id = "'+nome_genero+'"';
-    
-    connection.query(query, function (error, results, fields) {
-  
-       if (error) throw error;
-       connection.end();
-       var contato =  '';
-       contato = 'ID =>'+results[0].id+'- Titulo =>'+results[0].titulo+'-Ano =>'+results[0].ano;
-       response.json({"fulfillmentText" : contato })
-    })     
 
-    
+  var intentName = request.body.queryResult.intent.displayName;
+
+  const db = require('sql')
+
+ const Cadastro = db.sequelize.define('usuarios',{
+  nome: {
+    type: db.Sequelize.STRING
+  },
+  email: {
+    type: db.Sequelize.STRING
   }
-// ---- - - - - - - -- -- - - - - - - - - - - -- - - - - - - - - --- - - - - - - - - - - - - --   
- 
-  else if ( intentName == "lancamentos"  ) 
-  {console.log('Entrou no intent -> lancamentos')  
-  
-   response.json({
-     "fulfillmentMessages" : [
-              {
-                "card": {
-                  "title": "Processo Seletivo",
-                  "subtitle": "Bem vindo ao nosso processo seletivo",
-                  "imageUri": "https://firebasestorage.googleapis.com/v0/b/universidade-3d7f8.appspot.com/o/processo%2Fprocesso-seletivo.png?alt=media&token=d5a89cec-1c07-4dad-9b44-7aaf098128bd"
-                }
-              },
-              {
-                "text" :{
-                   "text": [
-                      "Voce quer participar do processo seletivo ?"
-                  ]
-                }
-              }
-            ]
-     });
-  
-  } // fim do else
- // ---- - - - - - - -- -- - - - - - - - - - - -- - - - - - - - - --- - - - - - - - - - - - - --   
-   
-  else if ( intentName == "lancamentos - yes"  ) 
-  {console.log('Entrou no intent -> lancamentos sim ')  
-  
-   response.json({
-     "fulfillmentMessages" : [
-              {
-                "text" :{
-                   "text": [
-                      "To procurando os lancamentos"
-                  ]
-                }
-              }
-            ]
-     });
-  
-  } // fim do else
-  
-// ---- - - - - - - -- -- - - - - - - - - - - -- - - - - - - - - --- - - - - - - - - - - - - --   
+})
 
-else if ( intentName == "bom-filme"  ) 
-  {console.log('Entrou no intent -> bom - filme ')  
-  
-   var query = 'select * from filme where avaliacao > 90';
-    
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+if ( intentName == "usuario-cadastro"  )
+  {  console.log('Entrou no intent -> cadastra-usuario')
+
+   Cadastro.create({
+     nome: request.body.queryResult.parameters['nome'],
+     email: request.body.queryResult.parameters['email']
+   }).then(function(){
+    response.json({"fulfillmentText" :"Você foi cadastrado com sucesso"})
+   }).catch(function(erro){
+    response.json({"fulfillmentText" :"Houve um erro no cadastro "})
+   })
+  }
+
+
+
+else if ( intentName == "usuario-login"  )
+  {
+    console.log('Entrou no intent -> usuario-login')
+    const email = request.body.queryResult.parameters['email'];
+
+    var id='0';
+    var query = 'select * from usuario where email = "'+email+'"';
     connection.query(query, function (error, results, fields) {
-  
        if (error) throw error;
+        var id_usuario = results.insertId;
        connection.end();
-       var cwontato =  '';
-       cwontato = 'Segundo os usuários do Google, o filme '+results[0].titulo+' tem uma aprovação de '+results[0].avaliacao+'%';
-      
-       response.json({"fulfillmentText" : cwontato })
-    })  
-     
-  } // fim do else
- // ---- - - - - - - -- -- - - - - - - - - - - -- - - - - - - - - --- - - - - - - - - - - - - --   
- 
-else if ( intentName == "bom-filme - yes"  ) 
-  {console.log('Entrou no intent -> bom - filme ')  
-  
-   var query = 'select * from filme where id = "'+nome_genero+'"';
-    
-    connection.query(query, function (error, results, fields) {
-  
-       if (error) throw error;
-       connection.end();
-       var contato =  '';
-       contato = 'ID =>'+results[0].id+'- Titulo =>'+results[0].titulo+'-Ano =>'+results[0].ano;
-       response.json({"fulfillmentText" : contato })
-    })  
-   
-   
-   response.json({
-     "fulfillmentMessages" : [
-              {
-                "card": {
-                  "title": "Processo Seletivo",
-                  "subtitle": "Bem vindo ao nosso processo seletivo",
-                  "imageUri": "https://firebasestorage.googleapis.com/v0/b/universidade-3d7f8.appspot.com/o/processo%2Fprocesso-seletivo.png?alt=media&token=d5a89cec-1c07-4dad-9b44-7aaf098128bd"
-                }
-              },
-              {
-                "text" :{
-                   "text": [
-                      "Quer que eu indique outro ?"
-                  ]
+       response.json({"fulfillmentText" :"Olá "+results[0].nome+", me diz qual gênero de filme você gosta:"})
+    });
+  }
+
+
+// ---- - - - - - - -- -- - - - - - - - - - - -- - - - - - - - - --- - - - - - - - - - - - - --
+  else if ( intentName == "filme"  )
+  {
+ console.log('Entrou no intent -> filme')
+    var id_usuario; var id_filme;
+    var nome_genero = request.body.queryResult.parameters['nome-genero'];
+
+    var query = 'select * from filme where genero = "'+nome_genero+'"';
+
+         connection.query(query, function(error, results, fields) {
+          if (error) throw error;
+              var id_filme = results[0].id;
+              var numRows = results.length - 1;
+
+              var json_texto = '{"fulfillmentMessages": [';
+
+              for (var i in results) {
+                json_texto += '{"card" :{"title": ["'+results[i].titulo+'"],"imageUri": ["'+results[i].imagem+'"],"subtitle": ["'+results[i].genero+'"]}},{"text" :{"text": ["'+results[i].descricao+'"]}},{"text" :{"text": ["Esse filme foi aprovado por '+results[i].avaliacao+'% dos usuários do Google"]}},{"text" :{"text": ["Aqui ta o link do trailer '+results[i].url+'"]}}';
+                    console.log(results[i])
+            if ( i < numRows ) {
+                  json_texto += ',';
                 }
               }
-            ]
-     });
-  
-  } // fim do else
-  
-});
 
+            json_texto += ']}';
+            response.send(json_texto);
+          });
+
+        // insert indicao na tabela
+        var nota='0';   var id='0';   var data ='0';
+         var query2 = 'insert into indicado values ("'+id_usuario+'","'+id_filme+'")';
+          connection.query(query2, function (error, results, fields) {
+         if (error) throw error;
+        console.log('Todo Id:' + results.insertId);
+         connection.end();
+    });
+
+  }
+// ---- - - - - - - -- -- - - - - - - - - - - -- - - - - - - - - --- - - - - - - - - - - - - --
+
+
+  else if ( intentName == "filme - no"  )
+  {
+ console.log('Entrou no intent -> filme')
+    var id_usuario; var id_filme;
+    var nome_genero = request.body.queryResult.parameters['nome-genero'];
+
+    var query = 'select * from filme where genero = "'+nome_genero+'"';
+
+         connection.query(query, function(error, results, fields) {
+          if (error) throw error;
+              var id_filme = results[0].id;
+              var numRows = results.length - 1;
+
+              var json_texto = '{"fulfillmentMessages": [';
+
+              for (var i in results) {
+                json_texto += '{"card" :{"title": ["'+results[i].titulo+'"],"imageUri": ["'+results[i].imagem+'"],"subtitle": ["'+results[i].genero+'"]}},{"text" :{"text": ["'+results[i].descricao+'"]}},{"text" :{"text": ["Esse filme foi aprovado por '+results[i].avaliacao+'% dos usuários do Google"]}},{"text" :{"text": ["Aqui ta o link do trailer '+results[i].url+'"]}}';
+                    console.log(results[i])
+            if ( i < numRows ) {
+                  json_texto += ',';
+                }
+              }
+
+            json_texto += ']}';
+            response.send(json_texto);
+          });
+
+        // insert indicao na tabela
+        var nota='0';   var id='0';   var data ='0';
+         var query2 = 'insert into indicado values ("'+id_usuario+'","'+id_filme+'")';
+          connection.query(query2, function (error, results, fields) {
+         if (error) throw error;
+        console.log('Todo Id:' + results.insertId);
+         connection.end();
+    });
+
+  }
+// ---- - - - - - - -- -- - - - - - - - - - - -- - - - - - - - - --- - - - - - - - - - - - - --
+
+
+
+else if ( intentName == "filme - no"  )
+  {
+ console.log('Entrou no intent -> filme')
+
+    var nome_genero = request.body.queryResult.parameters['nome-genero'];
+
+    var query = 'select * from filme where genero = "'+nome_genero+'"';
+
+         connection.query(query, function(error, results, fields) {
+          if (error) throw error;
+
+
+
+              var numRows = results.length - 1;
+
+              var json_texto = '{"fulfillmentMessages": [';
+
+              for (var i in results) {
+                json_texto += '{"card" :{"title": ["'+results[i].titulo+'"],"imageUri": ["'+results[i].imagem+'"],"subtitle": ["'+results[i].genero+'"]}},{"text" :{"text": ["'+results[i].descricao+'"]}},{"text" :{"text": ["Esse filme foi aprovado por '+results[i].avaliacao+'% dos usuários do Google"]}},{"text" :{"text": ["Aqui ta o link do trailer '+results[i].url+'"]}}';
+                    console.log(results[i])
+            if ( i < numRows ) {
+                  json_texto += ',';
+                }
+              }
+            connection.end();
+            json_texto += ']}';
+            response.send(json_texto);
+          });
+
+
+
+
+  }
+// ---- - - - - - - -- -- - - - - - - - - - - -- - - - - - - - - --- - - - - - - - - - - - - --
+
+
+
+else if ( intentName == "indicar-filme"  )
+  {
+ console.log('Entrou no intent -> indicar-filme')
+
+    var nome_genero = request.body.queryResult.parameters['nome-genero'];
+
+    var query = 'select * from filme where genero = "'+nome_genero+'" ';
+
+         connection.query(query, function(error, results, fields) {
+          if (error) throw error;
+
+              var numRows = results.length - 1;
+
+               var id_filme = results[i].id;
+
+              var json_texto = '{"fulfillmentMessages": [';
+
+              for (var i in results) {
+                json_texto += '{"card" :{"title": ["'+results[i].titulo+'"],"imageUri": ["'+results[i].imagem+'"],"subtitle": ["'+results[i].genero+'"]}},{"text" :{"text": ["'+results[i].descricao+'"]}},{"text" :{"text": ["Esse filme foi aprovado por '+results[i].avaliacao+'% dos usuários do Google"]}},{"text" :{"text": ["Aqui ta o link do trailer '+results[i].url+'"]}}';
+                    console.log(results[i])
+            if ( i < numRows ) {
+                  json_texto += ',';
+                }
+              }
+
+            connection.end();
+            json_texto += ']}';
+            response.send(json_texto);
+          });
+
+
+
+
+
+
+  }
+
+// cadastro de usuario
+
+
+// ---- - - - - - - -- -- - - - - - - - - - - -- - - - - - - - - --- - - - - - - - - - - - - --
+else if ( intentName == "trailer"  )
+  {
+ console.log('Entrou no intent trailer')
+
+    var nome_titulo = request.body.queryResult.parameters['nome-titulo'];
+
+    var query = 'select * from filme where titulo = "'+nome_titulo+'" ';
+
+         connection.query(query, function(error, results, fields) {
+          if (error) throw error;
+
+              var numRows = results.length - 1;
+
+              var json_texto = '{"fulfillmentMessages": [';
+
+              for (var i in results) {
+                json_texto += '{"card" :{"title": ["'+results[i].titulo+'"],"imageUri": ["'+results[i].imagem+'"],"subtitle": ["'+results[i].genero+'"]}},{"text" :{"text": ["Aqui ta o link do trailer '+results[i].url+'"]}}';
+                    console.log(results[i])
+            if ( i < numRows ) {
+                  json_texto += ',';
+                }
+              }
+
+            connection.end();
+            json_texto += ']}';
+            response.send(json_texto);
+          });
+
+  }
+
+
+// ---- - - - - - - -- -- - - - - - - - - - - -- - - - - - - - - --- - - - - - - - - - - - - --
+
+
+
+})
 
 
 // listen for requests :)
