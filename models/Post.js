88c79077487e4fce1359a28eddcc12929daeb3a0
Deusdete Filho <deusdetefilho@gmail.com>
2019-04-06 23:09:25 -0300

    Atualizacoes 2

---
+++ models/Post.js
@@ -0,0 +1,12 @@
+const db = require('./db')
+
+const Post = db.sequelize.define('usuarios',{
+ nome: {
+   type: db.Sequelize.STRING
+ },
+ email: {
+   type: db.Sequelize.STRING
+ }
+})
+
+module.exports = Post
